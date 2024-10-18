e89118e953681227942ee9b03cf13330efa0f855
SalemGebru <157522948+SalemGebru@users.noreply.github.com>
2024-10-18 09:24:35 +0300

    Delete landingpage.js

--- landingpage.js
+++
@@ -1,7 +0,0 @@
-const menuElements = document.querySelectorAll('.nav-bar ul li a');
-
-menuElements.forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.add('clicked');
-    });
-});
